c796e16c343928ed6a8acbb02ce7430d4d11d7c8
jo75nagy <>
2025-11-27 19:34:53 +0000

    Challenge: Solo Coding Capstone

--- loops/challenge_solo_coding_capstone/main.py
+++ loops/challenge_solo_coding_capstone/main.py
@@ -4,16 +4,14 @@ inventory = {
     "Eggs": [225, 2.12, 1.99],  # Eggs should be sold at a discount
     "Apples": [9, 1.50, 1.35]   # Apples need to be restocked
 }
-for item_name in inventory:
-    current_stock,reg_price,discounted_price = inventory[item_name]
-    #inventory[item_name][0] = current_stock
-    #inventory[item_name][1] = reg_price
-    #inventory[item_name][2] = discounted_price
+for item_name in inventory:  # item_name -> ("Az elsö elem a sorban utäna egy (:) itt "Bread": !! ")
+    current_stock,reg_price,discounted_price = inventory[item_name] # adatot a feldolgozásra kivenni [.., ..  ]
+    
     if current_stock < 30:
         print(f"{item_name} need restocking.")
-    elif current_stock > 100:
-        print(f"{item_name} should be sold at discounted price of {discounted_price}.")
-    elif current_stock >= 30 and current_stock <= 100:
+    if current_stock > 100:
+        print(f"{item_name} should be sold at the discounted price of {discounted_price}.") # elöhivashoz: { Var}
+    if current_stock >= 30 and current_stock <= 100:
         print(f"{item_name} should be sold at the regular price of {reg_price}.")
 
 
